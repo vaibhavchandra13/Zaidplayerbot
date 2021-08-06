69fe8d9f1dcc3f8a3c0d44986f10eadc6686e3d7
Itsunknown-12 <83804937+Itsunknown-12@users.noreply.github.com>
2021-08-06 16:27:36 +0530

    Update jiosaavn.ts

--- src/handlers/jiosaavn.ts
+++ src/handlers/jiosaavn.ts
@@ -5,19 +5,19 @@ import { commandExtractor } from '../utils';
 import { JioSaavnSongResponse, JioSaavnSongSearchResponse } from '../types/jiosaavn.response';
 import { playOrQueueSong } from '../tgcalls';
 
-export const JioSaavnPlay = Composer.command(['jiosaavn', 'jsvn'], async (ctx) => {
+export const JioSaavnPlay = Composer.command(['jiosaavn', 'play'], async (ctx) => {
     
     await ctx.telegram.sendChatAction(ctx.chat.id, "typing");
 
-    if (ctx.chat.type === 'private') return await ctx.reply("This Command works on Group Only");
+    if (ctx.chat.type === 'private') return await ctx.reply("ᴛʜɪꜱ ᴄᴏᴍᴍᴀɴᴅꜱ ꜰᴏʀ ᴏɴʟʏ ɢʀᴘꜱ");
 
     let { args: keyword } = commandExtractor(ctx.message.text);
-    if (!keyword) return await ctx.reply("Please Provide Search Keyword")
+    if (!keyword) return await ctx.reply("ᴘʟᴢ ᴛʏᴘᴇ ꜱᴏᴍᴇᴛʜɪɴɢ")
 
     let query = stringify({ query: keyword.replace(/\s/g, '+') });
     let resp = (await axios.get<JioSaavnSongSearchResponse[]>(`https://jiosaavn-api.vercel.app/search?${query}`)).data;
 
-    if (!resp[0]) return await ctx.reply("No Results Found");
+    if (!resp[0]) return await ctx.reply("ᴋᴜᴄʜʜ ɴ ᴍɪʟᴀ");
 
     let [result] = resp;
     let song = (await axios.get<JioSaavnSongResponse>(`https://jiosaavn-api.vercel.app/song?id=${result.id}`)).data;
